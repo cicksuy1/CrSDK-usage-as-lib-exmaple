4b03469561fbcc997a5775703c064241182ef13b
cicksuy1 <xxlostangle@gmail.com>
2024-03-28 13:18:22 +0200

    First Init Example of cr sdk usage

---
+++ src/main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+
+#include <cstdlib>
+#if defined(USE_EXPERIMENTAL_FS)
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#else
+#include <filesystem>
+namespace fs = std::filesystem;
+#if defined(__APPLE__)
+#include <unistd.h>
+#endif
+#endif
+
+#include <cstdint>
+#include <iomanip>
+#include "SonySDK/app/CRSDK/CameraRemote_SDK.h"
+#include "SonySDK/app/CameraDevice.h"
+// #include "CameraDevice.h"
+#include "SonySDK/app/Text.h"
+
+//#define LIVEVIEW_ENB
+
+#define MSEARCH_ENB
+namespace SDK = SCRSDK;
+
+
+
+
+int main() {
+        // Change global locale to native locale
+    std::locale::global(std::locale(""));
+
+    // Make the stream's locale the same as the current global locale
+    cli::tin.imbue(std::locale());
+    cli::tout.imbue(std::locale());
+
+    cli::tout << "RemoteSampleApp v1.11.00 running...\n\n";
+
+    CrInt32u version = SDK::GetSDKVersion();
+    int major = (version & 0xFF000000) >> 24;
+    int minor = (version & 0x00FF0000) >> 16;
+    int patch = (version & 0x0000FF00) >> 8;
+    // int reserved = (version & 0x000000FF);
+
+    cli::tout << "Remote SDK version: ";
+    cli::tout << major << "." << minor << "." << std::setfill(TEXT('0')) << std::setw(2) << patch << "\n";
+
+    cli::tout << "Initialize Remote SDK...\n";
+    cli::tout << "Working directory: " << fs::current_path() << '\n';
+
+    auto init_success = SDK::Init();
+    if (!init_success) {
+        cli::tout << "Failed to initialize Remote SDK. Terminating.\n";
+        SDK::Release();
+        std::exit(EXIT_FAILURE);
+    }
+    cli::tout << "Remote SDK successfully initialized.\n\n";
+
+
+
+    cli::tout << "Enumerate connected camera devices...\n";
+    SDK::ICrEnumCameraObjectInfo* camera_list = nullptr;
+    auto enum_status = SDK::EnumCameraObjects(&camera_list);
+    if (CR_FAILED(enum_status) || camera_list == nullptr) {
+        cli::tout << "No cameras detected. Connect a camera and retry.\n";
+        SDK::Release();
+        std::exit(EXIT_FAILURE);
+    }
+    auto ncams = camera_list->GetCount();
+    cli::tout << "Camera enumeration successful. " << ncams << " detected.\n\n";
+
+    for (CrInt32u i = 0; i < ncams; ++i) {
+        auto camera_info = camera_list->GetCameraObjectInfo(i);
+        cli::text conn_type(camera_info->GetConnectionTypeName());
+        cli::text model(camera_info->GetModel());
+        cli::text id = TEXT("");
+        if (TEXT("IP") == conn_type) {
+            cli::NetworkInfo ni = cli::parse_ip_info(camera_info->GetId(), camera_info->GetIdSize());
+            id = ni.mac_address;
+        }
+        else id = ((TCHAR*)camera_info->GetId());
+        cli::tout << '[' << i + 1 << "] " << model.data() << " (" << id.data() << ")\n";
+    }
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
